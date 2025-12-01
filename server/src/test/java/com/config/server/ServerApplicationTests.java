1540a859caeca370fda5c6fcb475ab143397d3e5
saksham gupta <guptasaksham2510@gmail.com>
2025-12-02 01:41:26 +0530

    sonarqube cofiguration

--- server/src/test/java/com/config/server/ServerApplicationTests.java
+++ server/src/test/java/com/config/server/ServerApplicationTests.java
@@ -2,8 +2,16 @@ package com.config.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.cloud.config.enabled=false",
+        "spring.cloud.config.server.bootstrap=false",
+        "spring.cloud.config.server.git.uri=file:./dummy",
+        "spring.cloud.config.server.native.search-locations=classpath:/dummy",
+        "eureka.client.enabled=false"
+})
 class ServerApplicationTests {
 
 	@Test
